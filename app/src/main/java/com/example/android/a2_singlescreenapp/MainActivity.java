5effc58d9e3ed4ced6e0a55df532c6d839abc542
koszojudit <koszojudit@Koszos-MacBook-Pro.local>
2017-04-10 00:12:08 +0200

    ScrollView, intents
    Making floating action buttons and contact info links clickable. Dial
    phone number and open new email intents  - methods added to code.
    Making the view scrollable by nesting in a ScrollView.

--- app/src/main/java/com/example/android/a2_singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/a2_singlescreenapp/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.android.a2_singlescreenapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import static android.R.attr.phoneNumber;
+import static com.example.android.a2_singlescreenapp.R.string.bizEmailMessage;
+import static com.example.android.a2_singlescreenapp.R.string.bizPhoneMessage;
+import static com.example.android.a2_singlescreenapp.R.string.bizPhoneNumber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void dialNumber (View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0036707777777"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void composeEmail (View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"colorandbeauty@gmail.com"});
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
 }
