49de4d46c93c4ee8e2ce632f1300ad8e954fa0fb
koszojudit <koszojudit@Koszos-MacBook-Pro.local>
2017-04-12 21:30:40 +0200

    Minor changes

--- app/src/main/java/com/example/android/a2_singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/a2_singlescreenapp/MainActivity.java
@@ -3,17 +3,8 @@ package com.example.android.a2_singlescreenapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
-import static android.R.attr.phoneNumber;
-import static com.example.android.a2_singlescreenapp.R.string.bizEmailMessage;
-import static com.example.android.a2_singlescreenapp.R.string.bizPhoneMessage;
-import static com.example.android.a2_singlescreenapp.R.string.bizPhoneNumber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void dialNumber (View view) {
+    public void dialNumber(View view) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:0036707777777"));
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -32,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void composeEmail (View view) {
+    public void composeEmail(View view) {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"colorandbeauty@gmail.com"});
